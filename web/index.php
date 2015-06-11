6c478e5967f36c6d31bb46b20041eb3e746c1422
namzug82 <gabriel.strato@gmail.com>
2015-06-11 07:46:42 +0200

    modified index.php, composer.lock and deleted src/Fw/Application.php

--- web/index.php
+++ web/index.php
@@ -3,11 +3,11 @@
 require '../vendor/autoload.php';
 
 use Fw\Application;
-//use Fw\YmlRouting;
+use Fw\YmlRouting;
 
 $app = new Application;
  
-// $routing = new YmlROuting;
-// $app->setRoutingComponent($routing);
+$routing = new YmlROuting;
+$app->setRoutingComponent($routing);
 
 $app->run();
