b02752a5658d50575164e39a447763254b3e8b1c
namzug82 <gabriel.strato@gmail.com>
2015-06-22 11:17:35 +0200

    modified index.php

--- web/index.php
+++ web/index.php
@@ -11,9 +11,9 @@ use Fw\Component\Routing\GenericParser;
 use Fw\Component\Routing\Router;
 use Fw\Component\Request\Request;
 use Fw\Component\Dispatcher\Dispatcher;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Fw\Component\View\JsonView;
 use Fw\Component\View\TwigView;
+use Fw\Component\Database\PDO;
 use Symfony\Component\Yaml\Parser;
 
 define("ROOT", __DIR__ . '/../src/App/Resources/config/routing/yaml/routing.yml');
@@ -26,11 +26,11 @@ $route = new GenericParser($yaml);
 
 $router = new Router($route);
 
-$request = new Request();
+$request = new Request;
 $requestPath = $request->getPath();
 $requestSubRoute = $router->getSubRouteName($requestPath);
 
-$dispatcher = new Dispatcher();
+$dispatcher = new Dispatcher;
 $controller = $dispatcher->getController($requestSubRoute);
 
 $invokeResponse = new $controller();
@@ -42,5 +42,26 @@ if ($response->getData() instanceof JsonResponse) {
     $view = new TwigView();
 }
 $view->render($response);
+
+
+$host = "localhost"; 
+$user = "root"; 
+$password = "1234"; 
+$db = "fw_db"; 
+
+$database = new PDO($db, $host, $user, $password);
+// $statement = $database->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
+// $statement->bindParam(':username', $username, \PDO::PARAM_STR);
+// $statement->bindParam(':password', $password, \PDO::PARAM_STR);
+// $statement->execute();
+
+// $username = 'manolo';
+// $password = '1234';
+// $statement = $database->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+// $statement->bindParam(':username', $username, \PDO::PARAM_STR);
+// $statement->bindParam(':password', $password, \PDO::PARAM_STR);
+// $statement->execute();
+
+// return $statement->fetch();
 
 //$app->run();
