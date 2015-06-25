190cc2d9ac74fcaa62d47765d84a56ec602dd162
namzug82 <gabriel.strato@gmail.com>
2015-06-25 11:47:39 +0200

    added routing.php

--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ require '../vendor/autoload.php';
 
 use Fw\Application;
 use Fw\Component\Routing\YamlParser;
+use Fw\Component\Routing\PhpParser;
 use Fw\Component\Routing\GenericParser;
 use Fw\Component\Routing\Router;
 use Fw\Component\Request\Request;
@@ -16,12 +17,16 @@ use Fw\Component\View\TwigView;
 use Fw\Component\Database\PDO;
 use Symfony\Component\Yaml\Parser;
 
-define("ROOT", __DIR__ . '/../src/App/Resources/config/routing/yaml/routing.yml');
+//define("ROOT", __DIR__ . '/../src/App/Resources/config/routing/yaml/routing.yml');
+define("ROOT", __DIR__ . '/../src/App/Resources/config/routing/php/routing.php');
+//define("ROOT", __DIR__ . '/../src/App/Resources/config/routing/json/routing.json');
 define("TEMPLATES", __DIR__ . '/../src/App/Resources/views');
 
 $parser = new Parser;
-$yaml = new YamlParser($parser, ROOT);
-$route = new GenericParser($yaml);
+//$yaml = new YamlParser($parser, ROOT);
+$php = new PhpParser(ROOT);
+//$route = new GenericParser($yaml);
+$route = new GenericParser($php);
 $router = new Router($route);
 $request = new Request;
 $dispatcher = new Dispatcher;
